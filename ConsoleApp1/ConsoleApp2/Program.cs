deaec6443c5142f54cce0b6e4792793eef972f3a
sophonwit19 <sophonwit.ka@kkumail.com>
2024-11-26 11:27:20 +0700

    add new project

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Sophonwit kaewsila";
+string student_id = "673450209-9";
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is" + name);
+Console.WriteLine("My student_id :" + student_id);
