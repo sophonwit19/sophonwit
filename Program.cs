1899e2cebbb78acbc996459ba080c92a6cc6b330
sophonwit19 <sophonwit.ka@kkumail.com>
2024-12-17 11:56:52 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Ittiphon Jannonsaeng";
+string Student_id = "673450043-7";
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name  is:" + name);
+Console.WriteLine("My student id is:" + Student_id);
